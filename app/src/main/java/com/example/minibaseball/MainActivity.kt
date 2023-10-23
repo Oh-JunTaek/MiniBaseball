54a549f6b5a5f2f349afb125ddc5dd3a33760af9
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-10-24 01:27:15 +0900

    minibaseball

--- app/src/main/java/com/example/minibaseball/MainActivity.kt
+++ app/src/main/java/com/example/minibaseball/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.minibaseball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.minibaseball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val userInput = "123" // 사용자 입력 값을 가져옵니다.
+        val result = checkGuess(userInput, target) // 'target'은 생성된 목표 숫자입니다.
+
+        // answer TextView 업데이트
+        binding.answer.text = "너가 말한 숫자\n${userInput}은\n${result.second}볼 ${result.first}스트라이크"
+
+        // board TextView 업데이트
+        binding.board.text = "${userInput}: ${result.second}볼 ${result.first}스트라이크"
     }
 }
