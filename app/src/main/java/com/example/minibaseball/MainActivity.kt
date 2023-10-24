13da8bdc3c43480246c8c9b9adff5db0d0d42318
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-10-24 22:36:33 +0900

    minibaseball

--- app/src/main/java/com/example/minibaseball/MainActivity.kt
+++ app/src/main/java/com/example/minibaseball/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.minibaseball
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextUtils
+import android.text.style.ForegroundColorSpan
 import com.example.minibaseball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -34,12 +39,34 @@ class MainActivity : AppCompatActivity() {
                 val result = checkGuess(userInput, target)
 
                 // answer TextView 업데이트
-                binding.answer.text =
-                    "너가 말한 숫자\n${userInput}은\n${result.second}볼 ${result.first}스트라이크"
+                val userInputText = SpannableString("너가 말한 숫자\n${userInput}\n의 결과는")
+                userInputText.setSpan(ForegroundColorSpan(Color.BLACK), 0, userInputText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                val ballInfo = SpannableString("\n${result.second}볼")
+                ballInfo.setSpan(ForegroundColorSpan(Color.GREEN), 0, ballInfo.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                val strikeInfo = SpannableString(" ${result.first}스트라이크")
+                strikeInfo.setSpan(ForegroundColorSpan(Color.YELLOW), 0, strikeInfo.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                binding.answer.text = TextUtils.concat(userInputText, ballInfo, strikeInfo)
 
                 // board TextView 업데이트
-                val prevText = if (binding.board.text.isEmpty()) "" else "${binding.board.text}\n"
-                binding.board.text = "$prevText${userInput}: ${result.second}볼 ${result.first}스트라이크"
+                val userInputTextBoard = SpannableString("${userInput}: ")
+                userInputTextBoard.setSpan(ForegroundColorSpan(Color.WHITE), 0, userInputTextBoard.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                val ballInfoBoard = SpannableString("${result.second}볼")
+                ballInfoBoard.setSpan(ForegroundColorSpan(Color.GREEN), 0, ballInfoBoard.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                val strikeInfoBoard = SpannableString(" ${result.first}스트라이크")
+                strikeInfoBoard.setSpan(ForegroundColorSpan(Color.YELLOW), 0,
+                    strikeInfoBoard.length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+
+                val prevText =
+                    if (binding.boardText.text.isEmpty()) "" else "${binding.boardText.text}\n"
+                binding.boardText.text =
+                    TextUtils.concat(prevText,userInputTextBoard ,ballInfoBoard ,strikeInfoBoard)
 
                 // 입력 초기화
                 inputNumber.fill(null)
@@ -50,15 +77,27 @@ class MainActivity : AppCompatActivity() {
                 ).forEach { it.setImageResource(R.drawable.baseline_question_mark_24) }
             }
         }
-    }
+//delete 버튼에 대한 기능 설정
+        binding.btndelete.setOnClickListener {
+            // 입력값 초기화
+            inputNumber.fill(null)
 
+            // 이미지 리소스 초기화
+            listOf(
+                binding.a1,
+                binding.a2,
+                binding.a3
+            ).forEach { it.setImageResource(R.drawable.baseline_question_mark_24) }
+        }
+    }
+//입력된 값 표기
             private fun addNumberToInput(number: Int) {
                 // 첫 번째 빈 위치 찾기
                 val position = inputNumber.indexOf(null)
                 if (position != -1) {
                     inputNumber[position] = number
 
-                    // 적절한 이미지로 업데이트하기 (여기서는 단순히 숫자로 표현함)
+                    // 이미지 불러오기(하단 getImageResource참조)
                     when (position) {
                         0 -> {
                             binding.a1.setImageResource(getImageResource(number)) }
@@ -72,10 +111,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            private fun getImageResource(number: Int): Int {
-                return when (number) {
-                    //...
-                    else -> R.drawable.baseline_question_mark_24
+            private fun getImageResource(number: Int): Int {//이미지 불러오기
+                val resourceName = "baseline_filter_${number}_24"
+                return resources.getIdentifier(resourceName, "drawable", packageName)
+
                 }
             }
         private fun generateTarget(): String {
@@ -96,4 +135,4 @@ class MainActivity : AppCompatActivity() {
 
             return Pair(strikes, balls)
         }
-    }
+
